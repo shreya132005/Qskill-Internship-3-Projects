29f10da8bb8b1e5cf63bbc2f335751e665015673
shreya132005 <shreyasrivastava1310@gmail.com>
2026-02-09 23:48:30 +0530

    Implement RealTimeGemBot with memory and live search

---
+++ project3/app/gemini.py
@@ -0,0 +1,13 @@
+from app.chat import chat_with_memory
+
+print("RealTimeGemBot – Gemini AI with Memory & Live Search")
+print("Type 'exit' to quit\n ")
+
+while True:
+    user_input = input("You: ")
+    
+    if user_input.lower()=="exit":
+        print("Goodbye")
+        break
+    reply = chat_with_memory(user_input)
+    print("AI:", reply)
